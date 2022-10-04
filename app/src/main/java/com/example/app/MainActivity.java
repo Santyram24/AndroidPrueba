5f4af4802b351604354344d30440dbef12c6dbcb
Santyram24 <Sramirez576@hotmail.com>
2022-10-03 21:58:08 -0500

    primer commit

---
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+    private EditText et1;
+    private EditText et2;
+    private TextView tv1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
