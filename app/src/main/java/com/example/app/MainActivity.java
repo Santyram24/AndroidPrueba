64836e06b3302fb79e896cd57e1ef1d74acf7095
Santyram24 <Sramirez576@hotmail.com>
2022-10-03 22:26:38 -0500

    Se añadio parte logica

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -15,5 +16,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et1= (EditText)findViewById(R.id.txt_num1);
+        et2=(EditText) findViewById(R.id.txt_num2);
+        tv1=(TextView) findViewById(R.id.txt_resul);
+
+    }
+
+    //Este metodo realiza la suma
+    public void sumar(View view){
+        String valor1= String.valueOf(et1.getText());
+        String valor2= String.valueOf(et2.getText());
+
+        int num1= Integer.parseInt(valor1);
+        int num2= Integer.parseInt(valor2);
+        int suma=num1+num2;
+
+        String resultado= String.valueOf(suma);
+        tv1.setText(resultado);
+
+
+
+
+
     }
 }
